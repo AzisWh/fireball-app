c66f4a2be373480a3d741657b49758a5f3155105
azisssss <aziswh7@gmail.com>
2024-10-11 13:22:22 +0700

    auth message

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,53 +29,53 @@
     </div>
   </section>                                  
 
-    <section class="mt-5">
-        <div class="container-fluid h-custom">
-          <div class="row d-flex justify-content-center align-items-center h-100">
+  <section class="mt-5">
+    <div class="container-fluid h-custom">
+        <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-md-9 col-lg-6 col-xl-5" data-aos="fade-right">
-              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                class="img-fluid" alt="Sample image">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    class="img-fluid" alt="Sample image">
             </div>
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1" data-aos="fade-left">
-              <form action="{{ route('login') }}" method="POST">
-                @csrf
-                <!-- Email input -->
-                <div data-mdb-input-init class="form-outline mb-4">
-                  <input type="email" id="email" name="email" class="form-control form-control-lg"
-                    placeholder="Enter a valid email address" />
-                  <label class="form-label" for="email">Email address</label>
-                </div>
-      
-                <!-- Password input -->
-                <div data-mdb-input-init class="form-outline mb-3">
-                  <input type="password" id="password" name="password" class="form-control form-control-lg"
-                    placeholder="Enter password" />
-                  <label class="form-label" for="password">Password</label>
-                </div>
-      
-                {{-- <div class="d-flex justify-content-between align-items-center">
-              
-                  <div class="form-check mb-0">
-                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                    <label class="form-check-label" for="form2Example3">
-                      Remember me
-                    </label>
-                  </div>
-                  <a href="#!" class="text-body">Forgot password?</a>
-                </div> --}}
-      
-                <div class="text-center text-lg-start mt-4 pt-2">
-                  <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
-                    style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
-                    <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? 
-                        <a href="{{url('/register')}}"class="link-danger">Register</a>
-                    </p>
-                </div>
-      
-              </form>
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <!-- Email input -->
+                    <div data-mdb-input-init class="form-outline mb-4">
+                        <input type="email" id="email" name="email" class="form-control form-control-lg"
+                            placeholder="Enter a valid email address" value="{{ old('email') }}" />
+                        <label class="form-label" for="email">Email address</label>
+                    </div>
+
+                    <!-- Password input -->
+                    <div data-mdb-input-init class="form-outline mb-3">
+                        <input type="password" id="password" name="password" class="form-control form-control-lg"
+                            placeholder="Enter password" />
+                        <label class="form-label" for="password">Password</label>
+                    </div>
+
+                    <!-- Error message for invalid login -->
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="text-center text-lg-start mt-4 pt-2">
+                        <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+                            style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                        <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? 
+                            <a href="{{ url('/register') }}" class="link-danger">Register</a>
+                        </p>
+                    </div>
+
+                </form>
             </div>
-          </div>
         </div>
-        
-      </section>
+    </div>
+</section>
+
 @endsection
